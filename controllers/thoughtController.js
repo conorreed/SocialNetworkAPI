af76f1ad59883bfc59f8a74ce242be2e1490fe49
Conor <conor.reed@gmail.com>
2023-11-15 09:39:52 -0600

    implemented the thought and user controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,89 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // GET to get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select('-__v')
+      .sort({ createdAt: -1 }) // Sort by createdAt in descending order
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // GET to get a single thought by its _id
+  getThoughtById(req, res) {
+    const { thoughtId } = req.params;
+
+    Thought.findById(thoughtId)
+      .select('-__v')
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'Thought not found' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // POST to create a new thought
+  createThought(req, res) {
+    const { thoughtText, username, userId } = req.body;
+
+    Thought.create({ thoughtText, username, userId })
+      .then((thought) => {
+        // Push the created thought's _id to the associated user's thoughts array field
+        return User.findByIdAndUpdate(
+          userId,
+          { $push: { thoughts: thought._id } },
+          { new: true, runValidators: true }
+        );
+      })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json({ thought, user });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // PUT to update a thought by its _id
+  updateThought(req, res) {
+    const { thoughtId } = req.params;
+    const { thoughtText } = req.body;
+
+    Thought.findByIdAndUpdate(
+      thoughtId,
+      { thoughtText },
+      { new: true, runValidators: true }
+    )
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'Thought not found' });
+        }
+        res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // DELETE to remove a thought by its _id
+  deleteThought(req, res) {
+    const { thoughtId } = req.params;
+
+    Thought.findByIdAndDelete(thoughtId)
+      .then((thought) => {
+        if (!thought) {
+          return res.status(404).json({ message: 'Thought not found' });
+        }
+        // Remove the thought's _id from the associated user's thoughts array field
+        return User.findByIdAndUpdate(
+          thought.userId,
+          { $pull: { thoughts: thoughtId } },
+          { new: true, runValidators: true }
+        );
+      })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json({ message: 'Thought and associated user data
---
+++ controllers/userController.js
@@ -0,0 +1,118 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  // GET all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v', // Exclude __v field from populated thoughts
+      })
+      .populate('friends', '-__v') // Exclude __v field from populated friends
+      .select('-__v')
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // GET a single user by ID and populated thought and friend data
+  getUserById(req, res) {
+    const { userId } = req.params;
+
+    User.findById(userId)
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate('friends', '-__v')
+      .select('-__v')
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // POST a new user
+  createUser(req, res) {
+    const { username, email } = req.body;
+
+    User.create({ username, email })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // PUT to update a user by ID
+  updateUser(req, res) {
+    const { userId } = req.params;
+    const { username, email } = req.body;
+
+    User.findByIdAndUpdate(
+      userId,
+      { username, email },
+      { new: true, runValidators: true }
+    )
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // DELETE to remove a user by ID
+  deleteUser(req, res) {
+    const { userId } = req.params;
+
+    User.findByIdAndDelete(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        // BONUS: Remove a user's associated thoughts when deleted
+        return Thought.deleteMany({ username: user.username });
+      })
+      .then(() => res.json({ message: 'User and associated thoughts deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // POST to add a new friend to a user's friend list
+  addFriend(req, res) {
+    const { userId, friendId } = req.params;
+
+    User.findByIdAndUpdate(
+      userId,
+      { $addToSet: { friends: friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // DELETE to remove a friend from a user's friend list
+  removeFriend(req, res) {
+    const { userId, friendId } = req.params;
+
+    User.findByIdAndUpdate(
+      userId,
+      { $pull: { friends: friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = userController;
