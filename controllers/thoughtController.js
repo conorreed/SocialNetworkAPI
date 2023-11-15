55f09944d122ec0d78a79c4666a6c7e7c61d301e
Conor <conor.reed@gmail.com>
2023-11-15 09:41:34 -0600

    did some more work on the thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -86,4 +86,47 @@ const thoughtController = {
         if (!user) {
           return res.status(404).json({ message: 'User not found' });
         }
-        res.json({ message: 'Thought and associated user data
+        res.json({ message: 'Thought and associated user data deleted' });
+    })
+    .catch((err) => res.status(500).json(err));
+},
+
+// POST to create a reaction stored in a single thought's reactions array field
+createReaction(req, res) {
+  const { thoughtId } = req.params;
+  const { reactionBody, username } = req.body;
+
+  Thought.findByIdAndUpdate(
+    thoughtId,
+    { $push: { reactions: { reactionBody, username } } },
+    { new: true, runValidators: true }
+  )
+    .then((thought) => {
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(thought);
+    })
+    .catch((err) => res.status(500).json(err));
+},
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+deleteReaction(req, res) {
+  const { thoughtId, reactionId } = req.params;
+
+  Thought.findByIdAndUpdate(
+    thoughtId,
+    { $pull: { reactions: { reactionId } } },
+    { new: true, runValidators: true }
+  )
+    .then((thought) => {
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(thought);
+    })
+    .catch((err) => res.status(500).json(err));
+},
+};
+
+module.exports = thoughtController;
